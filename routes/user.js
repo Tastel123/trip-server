760740be06ed730a69d1432283c1a33e3b01085a
Tastel123 <244674264@qq.com>
2019-07-25 17:02:22 +0800

    1

---
+++ routes/user.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var router = express.Router()
+var mysql = require('mysql')
+
+var data = {
+    code: 200,
+    msg: 'success'
+}
+
+// 创建mysql连接
+var pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'trip'
+})
+// 开始请求
